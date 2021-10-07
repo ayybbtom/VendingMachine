0c25c8ad43423535708f2dbdef3b7366377ab877
John Bernstein <john.r.bernstein@gmail.com>
2021-10-07 11:21:01 -0400

    Progress on class skeleton. Will continue building remaining product subclasses.

---
+++ dotnet/Capstone/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    public abstract class Product
+    {
+        public string Sound { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string SlotLocation { get; set; }
+
+        public Product(string sound, decimal price, string name, string slotLocation)
+        {
+            Sound = sound;
+            Price = price;
+            Name = name;
+            SlotLocation = slotLocation;
+        }
+
+    }
+}
